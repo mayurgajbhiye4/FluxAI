6109238937f8e7e0be364d652529faa9f5ea9335
Mayur Gajbhiye <mayurgajbhiye4@gmail.com>
2025-05-05 14:20:16 +0530

    removed /static/ dependency from backend

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/dist')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/dist/assets')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
