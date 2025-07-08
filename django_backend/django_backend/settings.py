ed2c997051d3d1cd67673b375c7074ef34f3efd9
Mayur Gajbhiye <mayurgajbhiye4@gmail.com>
2025-07-08 17:52:34 +0530

    updated staticfiles DIRS

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -155,9 +155,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/dist/assets')]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/dist/assets')]
+else:
+    STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
